a280f55403a2a6a71e927cfa430e147bbb94c94e
vhsvhs <victorhansonsmith@gmail.com>
2014-12-12 12:03:26 -0800

    .

--- fogasrdb.py
+++ fogasrdb.py
@@ -1,5 +1,6 @@
 import sqlite3 as lite
 import os, sys
+from tools import *
 
 def build_db(dbpath = None):
     """Initializes all the tables. Returns the DB connection object.
@@ -18,7 +19,7 @@ def build_db(dbpath = None):
     cur.execute("create table if not exists species(id INTEGER primary key autoincrement, name TEXT unique)")
     cur.execute("create table if not exists Log(id INTEGER primary key, time DATETIME DEFAULT CURRENT_TIMESTAMP,  message TEXT, code INT)")
     cur.execute("create table if not exists ErrorLog(id INTEGER primary key, time DATETIME DEFAULT CURRENT_TIMESTAMP,  message TEXT, code INT)")
-    cur.execute("create table if not exists seqnames(id integer primary key autoincrement, speciesid INTEGER, name TEXT unique)")
+    cur.execute("create table if not exists seqnames(id integer primary key autoincrement, speciesid INTEGER, name TEXT)")
     cur.execute("create table if not exists ntseqs(id INTEGER primary key autoincrement, speciesid INT, sequence TEXT, seqid INT)")
     cur.execute("create table if not exists aaseqs(id INTEGER primary key autoincrement, speciesid INT, sequence TEXT, seqid INT)")
     cur.execute("create table if not exists orthogroup(id INTEGER primary key autoincrement, name TEXT unique)")
@@ -26,12 +27,6 @@ def build_db(dbpath = None):
     con.commit()
     return con
     
-"""
-log.py - a logging system for the ASR pipeline
-"""
-import os
-from tools import *
-
 def write_log(con, message, code=None):
     """
     Writes to the log file
@@ -62,3 +57,13 @@ def write_error(con, message, code=None):
     cur.execute(sql)
     con.commit()
     print "\n. ERROR: " + message
+    
+def get_species_name(con, speciesid):
+    cur = con.cursor()
+    sql = "select name from species where id=" + speciesid.__str__()
+    x = cur.fetchall()
+    if x.__len__() == 0:
+        return None
+    else:
+        return x[0][0]
+    
--- tools.py
+++ tools.py
@@ -42,3 +42,148 @@ def read_fasta(fpath):
     lines = fin.readlines()
     fin.close()
     return read_fastalines( lines )
+
+
+def get_taxon_urls():                          
+    sock = urllib.urlopen("http://www.broadinstitute.org/regev/orthogroups/sources.html") 
+    htmlSource = sock.read()                            
+    sock.close()                                        
+    
+    taxon_aaurl = {}
+    taxon_nturl = {}
+    
+    for l in htmlSource.split("\n"):
+        taxon = ""
+        aaurl = ""
+        nturl = ""
+        if l.__contains__("AA</a>"):
+            """Taxon Name"""
+            x = l.find("<i>")
+            if x != -1:
+                start = x+3
+                for c in l[start:]:
+                    if c == "<":
+                        break
+                    else:
+                        taxon += c
+                        
+            """AA URL"""
+            x = l.find("href=aa/")
+            if x != -1:
+                start = x+5
+                for c in l[start:]:
+                    if c == ">":
+                        break
+                    else:
+                        aaurl += c
+                                    
+            """NT URL"""
+            x = l.find("href=nt/")
+            if x != -1:
+                start = x+5
+                for c in l[start:]:
+                    if c == ">":
+                        break
+                    else:
+                        nturl += c             
+            #print taxon, aaurl, nturl
+            if taxon.__len__() > 1 and aaurl.__len__() > 1 and nturl.__len__() > 1:
+                taxon_aaurl[taxon] = aaurl
+                taxon_nturl[taxon] = nturl
+    return (taxon_aaurl, taxon_nturl)
+        
+def get_fasta_source(url, con):
+    sock = urllib.urlopen("http://www.broadinstitute.org/regev/orthogroups/" + url) 
+    htmlSource = sock.read()                            
+    sock.close() 
+    taxa_seq = read_fastalines( htmlSource.split() )
+    write_log(con, "I found " + taxa_seq.__len__().__str__() + " sequences in " + url)
+    return taxa_seq
+
+
+def import_sequences(con):
+    cur = con.cursor()
+    cur.execute("delete from species")
+    con.commit()
+    cur.execute("delete from seqnames")
+    con.commit()
+    cur.execute("delete from aaseqs")
+    con.commit()
+    cur.execute("delete from ntseqs")
+    con.commit()
+    
+    (taxon_aaurl, taxon_nturl) = get_taxon_urls()
+    for taxon in taxon_aaurl:
+        sql = "select count(*) from species where name='" + taxon + "'"
+        cur.execute(sql)
+        count = cur.fetchone()[0]
+        if count == 0:
+            sql = "insert into species (name) values('" + taxon + "')"
+            cur.execute(sql)
+            con.commit()    
+        sql = "select id from species where name='" + taxon + "'"
+        cur.execute(sql)
+        speciesid = cur.fetchone()[0]
+        
+        print taxon, taxon_aaurl[taxon]
+        
+        """aa"""
+        taxa_seq = get_fasta_source( taxon_aaurl[taxon], con)
+        for t in taxa_seq:
+            sql = "select count(*) from seqnames where speciesid=" + speciesid.__str__() + " and name='" + t + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count == 0:
+                sql = "insert into seqnames (speciesid, name) values(" + speciesid.__str__() + ",'" + t + "')"
+                cur.execute(sql)
+                con.commit()
+            else:
+                write_log("Warning: I found a previous copy an AA sequence named " + t)
+            
+            sql = "select id from seqnames where speciesid=" + speciesid.__str__() + " and name='" + t + "'"
+            cur.execute(sql)
+            seqid = cur.fetchone()[0]
+            
+            sql = "insert into aaseqs (speciesid, sequence, seqid) values("
+            sql += speciesid.__str__() + ",'" + taxa_seq[t] + "'," + seqid.__str__() + ")"
+            cur.execute(sql)
+        con.commit()
+        
+        """nt"""
+        taxa_seq = get_fasta_source( taxon_nturl[taxon], con)
+        for t in taxa_seq:
+            sql = "select count(*) from seqnames where name='" + t + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count == 0:
+                """We're expecting to already have seen this sequence name in the AA data"""
+                write_log(con, "I can't find the sequence " + t + ".")
+                sql = "delete from seqnames where name='" + t + "'"
+                cur.execute(sql)
+                con.commit()
+                continue
+            
+            sql = "select id from seqnames where speciesid=" + speciesid.__str__() + " and name='" + t + "'"
+            cur.execute(sql)
+            seqid = cur.fetchone()[0]
+            
+            sql = "insert into ntseqs (speciesid, sequence, seqid) values("
+            sql += speciesid.__str__() + ",'" + taxa_seq[t] + "'," + seqid.__str__() + ")"
+            cur.execute(sql)
+        con.commit()
+
+
+def verify_sequences(con):
+    cur = con.cursor()
+    sql = "select id from species"
+    cur.execute(sql)
+    x = cur.fetchall()
+    for ii in x:
+        speciesid = ii[0]
+        speciesname = get_species_name(con, speciesid)
+        sql = "select count(*) from seqnames where speciesid=" + speciesid.__str__()
+        cur.execute(sql)
+        count = cur.fetchone()[0]
+        write_log("Species " + speciesname + " has " + count.__str__() + " gene sequences.")
+        
+           
